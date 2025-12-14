234651a0154fb241132e475ee625bc439824fa9f
shubham <shubhamgajjewar@gmail.com>
2025-12-14 21:36:06 +0530

    deployment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello from Jenkins CI pipeline!");
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Jenkins CI pipeline!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
